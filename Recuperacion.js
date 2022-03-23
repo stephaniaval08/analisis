970870ec15c078ab8ad06163bdde538f689eb680
stephania <vale.ahumada1007@gmail.com>
2022-03-23 08:14:48 -0500

    solucion recuperacion

---
+++ Recuperacion.js
@@ -0,0 +1,35 @@
+var tiempo;
+var litros;
+litros=prompt("ingrese una cantidad en litros");
+tiempo=litros*1/145
+console.log("la piscina tarda en llenarse",tiempo);
+
+var milimetros;
+var metros;
+metros=prompt("ingrese una cantidad en metros");
+milimetros=metros*1000;
+console.log("La equivalencia en mililitros es", milimetros);
+
+var decimetros;
+decimetros=milimetros/100;
+console.log("La equivalencia en decimetros es", decimetros);
+
+var kilometros;
+kilometros=decimetros/10000;
+console.log("La equivalencia en kilometros es", kilometros);
+
+var horas;
+var dinero;
+horas=prompt("ingrese duración en llamada");
+dinero=horas*60*120;
+console.log("el costo de la llamada es", dinero);
+
+var horas;
+var costo;
+horas= prompt("ingrese la cantidad de horas que desea ver");
+costo= (horas - 1) *(25000) + 40000;
+console.log("el costo de las clases es",costo);
+
+
+
+
---
+++ Repaso.js
@@ -0,0 +1,20 @@
+var metros 
+var kilometros 
+metros=prompt("ingrese un valor en metros");
+kilometros=metros/1000
+console.log("La equivalencia de metros a km es",kilometros);
+
+var litros;
+var distancia;
+distancia=prompt("ingrese un valor de una distancia");
+litros=distancia*290/100
+console.log("el total de combustible que gasta es",litros);
+
+var kilolitros;
+kilolitros=litros/1000;
+console.log("La equivalencia en kilolitros es", kilolitros);
+
+
+
+
+
